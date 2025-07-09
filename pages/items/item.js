fb1e6724b3594783b7446d13bb8000466c1f777b
deelaka-ransilu <ransilu.deelaka@gmail.com>
2025-07-09 06:19:08 +0530

    item list added

--- pages/items/item.js
+++ pages/items/item.js
@@ -1,15 +1,3 @@
-let itemsList = [
-    {
-        category:"burgers",
-        itemCode:"B1001",
-        itemName:"Classic Burger (Large)",
-        price:750.00,
-        discount:0,
-        qty:10,
-        expDate:"07-10-2025"
-    }
-]
-
-function searchBtnOnClick(){
+function loadItems(){
     
 }
